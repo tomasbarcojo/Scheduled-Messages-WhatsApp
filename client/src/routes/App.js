5c80420fc87942f5c00528431e1053f85e7d45df
Tomas <tomasbarcojo@gmail.com>
2020-11-14 16:58:55 -0300

    First route for front

--- client/src/routes/App.js
+++ client/src/routes/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import Home from '../components/Home/Home'
 
-function App() {
+const App = () => {
   return (
     <Router>
         <Switch>
