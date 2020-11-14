388955b08db8225bd405d29016407c5e4a64334b
Tomas <tomasbarcojo@gmail.com>
2020-11-14 17:26:25 -0300

    Added some styles for home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='OuterContainer'>
+            <div className='InnerContainer'>
+                <h1>Home</h1>
+            </div>
         </div>
     )
 }
