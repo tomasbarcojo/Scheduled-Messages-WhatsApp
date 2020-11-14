b43e920560d8383af77f48865f240e8dd280969c
Tomas Barcojo <tomasbarcojo@gmail.com>
2020-11-14 20:34:48 -0300

    Working on home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,7 +5,11 @@ export default function Home() {
     return (
         <div className='OuterContainer'>
             <div className='InnerContainer'>
-                <h1>Home</h1>
+                <h1 className='heading'>Sign In</h1>
+                <div className='Input'>
+                    <span className='descInputs'>User:</span>
+                    <input className='Input' type='text' placeholder='Type something'/>
+                </div>
             </div>
         </div>
     )
