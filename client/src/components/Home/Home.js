9968a890eb7b5d756739ab830bea0f10c450d8cb
Tomas Barcojo <tomasbarcojo@gmail.com>
2020-11-16 12:35:59 -0300

    Home page almost done

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 export default function Home() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <div className='OuterContainer'>
             <div className='InnerContainer'>
-                <h1 className='heading'>Sign In</h1>
-                <div className='Input'>
-                    <span className='descInputs'>User:</span>
-                    <input className='Input' type='text' placeholder='Type something'/>
+                <h1 className='heading'>Welcome!</h1>
+                <div>
+                    <input
+                    className='Input' 
+                    type='text'
+                    placeholder='Email'
+                    onChange={e => setEmail(e.target.value)}
+                    />
                 </div>
+                <div>
+                    <input 
+                    className='Input mt-20' 
+                    type='password' 
+                    placeholder='Password'
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                </div>
+                <Link /*onClick={dispatch()}*/ to='/dashboard'>
+                    <button className='button mt-20' type="submit">Log In</button>
+                </Link>
             </div>
         </div>
     )
