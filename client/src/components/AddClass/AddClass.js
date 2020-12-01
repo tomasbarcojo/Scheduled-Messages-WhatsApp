3b2cfc2c4f4743eecca6d61f3b9694ff71589684
Tomas Barcojo <tomasbarcojo@gmail.com>
2020-12-01 07:17:12 -0300

    Validation and alerts on add class panel

--- client/src/components/AddClass/AddClass.js
+++ client/src/components/AddClass/AddClass.js
@@ -2,12 +2,31 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import NavBar from '../NavBar/NavBar'
 import { newClass } from '../../actions/classes'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
 import './AddClass.css'
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }));
 
 export default function AddClass() {
     const dispatch = useDispatch()
     const token = localStorage.getItem('token')
     const user = useSelector(state => state.user)
+    const [openSuccess, setOpenSuccess] = useState(false);
+    const [openError, setOpenError] = useState(false);
+    const classes = useStyles();
     const [classData, setclassData] = useState({
         description: '',
         url: '',
@@ -16,19 +35,41 @@ export default function AddClass() {
         userId: user.id
     })
 
+    
+    const resetForm = () => {
+        setclassData({
+            description: '',
+            url: '',
+            start: '',
+            end: ''
+        })
+    }
+    
     const handleSubmit = (e) => {
         e.preventDefault()
-        dispatch(newClass(classData, token))
+        if (classData.description && classData.url && classData.start && classData.end) {
+            dispatch(newClass(classData, token))
+            setOpenSuccess(true); //to open the success message
+            resetForm()
+        }
+        else {
+            setOpenError(true)
+        }
     }
-
-    const resetForm = () => {
-
-    }
-
+    
     const handleChange = (event) => {
         setclassData({...classData, [event.target.name]: event.target.value})
     }
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+
+        setOpenSuccess(false)
+        setOpenError(false)
+    };
+    
     return (
         <div>
             <NavBar />
@@ -43,10 +84,10 @@ export default function AddClass() {
                             className='Input'
                             type='text'
                             placeholder='Description'
+                            value={classData.description}
                             onChange={handleChange}
                             />
                         </div>
-                        {/* {showErr && errors.firstName && (<p className='danger'>{errors.firstName}</p>)} */}
                         <div className='dataRow'>
                             <p>Url:</p>
                             <input
@@ -54,10 +95,10 @@ export default function AddClass() {
                             className='Input'
                             type='text'
                             placeholder='URL'
+                            value={classData.url}
                             onChange={handleChange}
                             />
                         </div>
-                        {/* {showErr && errors.lastName && (<p className='danger'>{errors.lastName}</p>)} */}
                         <div className='dataRow'>
                             <p>Starting day and time:</p>
                             <input
@@ -65,10 +106,10 @@ export default function AddClass() {
                             className='Input'
                             type='datetime-local'
                             placeholder='Start time'
+                            value={classData.start}
                             onChange={handleChange}
                             />
                         </div>
-                        {/* {showErr && errors.email && (<p className='danger'>{errors.email}</p>)} */}
                         <div className='dataRow'>
                             <p>End time:</p>
                             <input
@@ -76,12 +117,24 @@ export default function AddClass() {
                             className='Input' 
                             type='datetime-local' 
                             placeholder='End time'
+                            value={classData.end}
                             onChange={handleChange}
                             />
                         </div>
-                        {/* {showErr && errors.password && (<p className='danger'>{errors.password}</p>)} */}
                         <button className='buttonRegister mt-20' type="submit">Add</button>
                     </form>
+
+                        <Snackbar open={openSuccess} autoHideDuration={6000} onClose={handleClose}>
+                            <Alert onClose={handleClose} severity="success">
+                                Class added!
+                            </Alert>
+                        </Snackbar>
+                        <Snackbar open={openError} autoHideDuration={6000} onClose={handleClose}>
+                            <Alert onClose={handleClose} severity="error">
+                                All inputs are required
+                            </Alert>
+                        </Snackbar>
+
                 </div>
             </div>
         </div>
