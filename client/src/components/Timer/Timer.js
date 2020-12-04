bfb144d517060c0747528f35dc9ea949a14ef5e5
Tomas Barcojo <tomasbarcojo@gmail.com>
2020-12-04 12:09:45 -0300

    Some test to execute function to start a class

---
+++ client/src/components/Timer/Timer.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+export default function Timer() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+
+    const updateTime = () => {
+        var auxtime = new Date().toLocaleTimeString()
+        setTime(auxtime)
+    }
+
+    setInterval(updateTime, 1000)
+
+    if (time === '11:58:45') {
+        alert('es la hora')
+    }
+
+
+    return (
+        <div>
+            <h1>{time}</h1>
+        </div>
+    )
+}
+
--- client/src/routes/App.js
+++ client/src/routes/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home/Home'
 import Register from '../components/Register/Register'
 import Dashboard from '../components/Dashboard/Dashboard'
+import Timer from '../components/Timer/Timer'
 // import MP from '../components/MercadoPago/MercadoPago'
 import AddClass from '../components/AddClass/AddClass'
 import { getUser } from '../actions/user'
@@ -18,23 +19,23 @@ const App = () => {
     dispatch(getUser(user.id, token))
   }
 
-  setInterval(() => {
-    if (listClasses) {
-      for (var i = 0; i < listClasses.length; i++) {
-          if (Date.parse(listClasses[i].start) < Date.now()) {
-              console.log(`Request for joining meet ${listClasses[i].url}`);
-              dispatch(startClass(listClasses[i].url))
-              // listClasses[i].start = listClasses[i].endTime + 2000;
-          }
-    //       if (Date.parse(listClasses[i].end) < Date.now()) {
-    //           console.log(`Request for leaving meet ${listClasses[i].url}`);
-    //           obj.end();
-    //           // delete listClasses[x]
-    //       }
-      }
-    }
-    console.log('Set interval ejecutado')
-  }, 1000)
+  // setInterval(() => {
+  //   if (listClasses) {
+  //     for (var i = 0; i < listClasses.length; i++) {
+  //         if (Date.parse(listClasses[i].start) < Date.now()) {
+  //             console.log(`Request for joining meet ${listClasses[i].url}`);
+  //             dispatch(startClass(listClasses[i].url))
+  //             // listClasses[i].start = listClasses[i].endTime + 2000;
+  //         }
+  //   //       if (Date.parse(listClasses[i].end) < Date.now()) {
+  //   //           console.log(`Request for leaving meet ${listClasses[i].url}`);
+  //   //           obj.end();
+  //   //           // delete listClasses[x]
+  //   //       }
+  //     }
+  //   }
+  //   console.log('Set interval ejecutado')
+  // }, 1000)
 
   return (
     <Router>
@@ -43,6 +44,7 @@ const App = () => {
             <Route exact path="/register" component={Register} />
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addclass" component={AddClass} />
+            <Route exact path="/profile" component={Timer} />
             {/* <Route exact path="/mercadopago" component={MP} /> */}
         </Switch>
     </Router>
